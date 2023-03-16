764c18ccfed059048ff9fe9a8196146ee413fd8d
shyammunde1 <shyammunde1@gmail.com>
2023-03-16 16:17:44 +0530

    redux use complted

---
+++ src/components/CartCard.js
@@ -0,0 +1,19 @@
+import { useDispatch } from "react-redux";
+import { remove } from "../store/CartSlice";
+import "./CartCard.css";
+
+export const CartCard = ({product}) => {
+
+ const dispatch= useDispatch()
+
+  const {name, price, image} = product;
+
+  return (
+      <div className="cartCard">
+        <img src={image} alt={name} />
+        <p className="productName">{name}</p>
+        <p className="productPrice">${price}</p>
+        <button onClick={()=> dispatch(remove(product))} >Remove</button>
+      </div>
+  )
+}
